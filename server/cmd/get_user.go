32bcfaa6abd328bcb052b754e14ba81eab2591e9
sota_masuda <sota.m777121@gmail.com>
2023-04-01 01:56:31 +0900

    successfully invoke lambda simple get request

--- server/cmd/get_user.go
+++ server/cmd/get_user.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"context"
+	"fmt"
+
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
@@ -16,13 +20,30 @@ type GetUserResponse struct {
 	GoogleId    string `json:"google_id"`
 }
 
-func GetUserHandler(request GetUserRequest) (GetUserResponse, error) {
-	return GetUserResponse{
-		UserId:      request.UserId,
+func GetUserHandler(ctx context.Context, req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	queryStringParameters := req.QueryStringParameters
+	fmt.Println(queryStringParameters)
+	pathParameters := req.PathParameters
+	fmt.Println(pathParameters)
+	headers := req.Headers
+	fmt.Println(headers)
+	body := req.Body
+	fmt.Println(body)
+
+	userId := pathParameters["id"]
+	fmt.Println("path parameter: id = " + userId)
+
+	respBody := GetUserResponse{
+		UserId:      "test_user_id",
 		UserName:    "test_user",
 		CompanyName: "test_company",
 		Authority:   "admin",
 		GoogleId:    "test_google_id",
+	}
+
+	return events.APIGatewayProxyResponse{
+		StatusCode: 200,
+		Body:       fmt.Sprintf("%v", respBody),
 	}, nil
 }
 
